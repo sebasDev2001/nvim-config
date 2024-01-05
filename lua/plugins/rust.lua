59eead5defbdbc437a43866d41eef0b250bab668
sebasDev2001 <sebastian.sierrav2001@gmail.com>
2024-01-05 15:42:59 -0500

    now use rustaceanvim since rust-tools archived

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -1,12 +1,17 @@
 return {
+  -- {
+  --   "simrat39/rust-tools.nvim",
+  --   ft = "rust",
+  --   dependencies = "neovim/nvim-lspconfig",
+  --
+  --   config = function(_, opts)
+  --     require("rust-tools").setup(opts)
+  --   end,
+  -- },
   {
-    "simrat39/rust-tools.nvim",
-    ft = "rust",
-    dependencies = "neovim/nvim-lspconfig",
-
-    config = function(_, opts)
-      require("rust-tools").setup(opts)
-    end,
+    "mrcjkb/rustaceanvim",
+    version = "^3",
+    ft = {"rust"},
   },
   {
     "rust-lang/rust.vim",
