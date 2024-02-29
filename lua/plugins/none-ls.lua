8a121c32260eae748cc2b276f0b8e94902e7fd59
sebasDev2001 <sebastian.sierrav2001@gmail.com>
2024-02-29 16:31:26 -0500

    eliminate dep formatters in none-ls

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,7 +7,6 @@ return {
     nl.setup({
       sources = {
         -- webdev stuff
-        b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
         b.formatting.prettier.with({
           filetypes = { "html", "markdown", "css", "json", "javascript", "typescript" },
         }), -- so prettier works only on these filetypes
@@ -15,11 +14,7 @@ return {
         -- Lua
         b.formatting.stylua,
 
-        -- cpp
-        b.formatting.clang_format,
-
         -- Python
-        b.diagnostics.ruff,
         b.formatting.black.with({
           extra_args = { "--line-length=120" },
         }),
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -22,8 +22,19 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight",
+      colorscheme = "nightfly",
     },
   },
+  -- bufferline
+  {
+    "akinsho/bufferline.nvim",
+    opts = {
+      options = {
+        mode = "tabs",
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+      },
+    },
+  }
 
 }
