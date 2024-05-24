e215b5fd9785720ec5873844d368f79b12eb4384
sebasDev2001 <sebastian.sierrav2001@gmail.com>
2024-05-23 19:27:19 -0500

    Now borders on snippets again

--- lua/plugins/luaSnip.lua
+++ lua/plugins/luaSnip.lua
@@ -1,4 +1,3 @@
-if true then return {} end
 return {
   {
     "hrsh7th/nvim-cmp",
@@ -7,13 +6,6 @@ return {
     },
     ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      end
-
-      local luasnip = require("luasnip")
       local cmp = require("cmp")
 
       opts.window = {
@@ -24,31 +16,6 @@ return {
       opts.experimental = {
         ghost_text = false,
       }
-
-      opts.mapping = vim.tbl_extend("force", opts.mapping, {
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true})
-          elseif require("copilot.suggestion").is_visible() then
-            require("copilot.suggestion").accept()
-          elseif luasnip.expand_or_locally_jumpable() then
-            luasnip.expand_or_jump()
-          elseif has_words_before() then
-            cmp.complete()
-          else
-            fallback()
-          end
-        end, {"i", "s"}),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item({ behavior = cmp.SelectBehavior.Insert })
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-      })
     end,
   },
 }
