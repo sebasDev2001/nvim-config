c2122161434bc0c330265056b6b011a281794225
sebasDev2001 <sebastian.sierrav2001@gmail.com>
2024-12-24 15:59:01 -0500

    Use blink

--- lua/plugins/luaSnip.lua
+++ lua/plugins/luaSnip.lua
@@ -1,21 +1,62 @@
 return {
   {
     "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-emoji",
-    },
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      local cmp = require("cmp")
-
-      opts.window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-      }
-
-      opts.experimental = {
-        ghost_text = false,
-      }
-    end,
+    enabled = false
+    -- dependencies = {
+    --   "hrsh7th/cmp-emoji",
+    -- },
+    -- ---@param opts cmp.ConfigSchema
+    -- opts = function(_, opts)
+    --   local cmp = require("cmp")
+    --
+    --   opts.window = {
+    --     completion = cmp.config.window.bordered(),
+    --     documentation = cmp.config.window.bordered(),
+    --   }
+    --
+    --   opts.experimental = {
+    --     ghost_text = false,
+    --   }
+    -- end,
   },
+  {
+    'saghen/blink.cmp',
+    -- optional: provides snippets for the snippet source
+    dependencies = 'rafamadriz/friendly-snippets',
+    version = '*',
+    opts = {
+      keymap = {
+        preset = 'default',
+
+        ['<Up>'] = { 'select_prev', 'fallback' },
+        ['<Down>'] = { 'select_next', 'fallback' },
+
+        ['<C-e>'] = {},
+
+        ['<C-space>'] = { function(cmp) cmp.show({ providers = { 'snippets' } }) end },
+
+        ['<C-n>'] = { 
+          function(cmp)
+            if some_condition then return end -- runs the next command
+            return true -- doesn't run the next command
+          end,
+          'select_next'
+        },
+        ['<CR>'] = { 'accept', 'fallback' },
+      },
+      appearance = { 
+        use_nvim_cmp_as_default = true,
+        nerd_font_variant = 'mono'
+      },
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'buffer' },
+      },
+      completion = {
+        ghost_text = {
+          enabled = false,
+        },
+      },
+    },
+    opts_extend = { "sources.default" },
+  }
 }
