32724f1cea29bae7f18641ccd2a497bc4e21a74b
sebasDev2001 <sebastian.sierrav2001@gmail.com>
2024-12-25 12:25:25 -0500

    make blink lsp config dependency

--- lua/plugins/lps.lua
+++ lua/plugins/lps.lua
@@ -3,6 +3,9 @@
 local util = require("lspconfig.util")
 return {
   "neovim/nvim-lspconfig",
+  dependencies = {
+    'saghen/blink.cmp',
+  },
   opts = {
     servers = {
       pyright = {
@@ -91,4 +94,11 @@ return {
       end,
     },
   },
+  config = function ()
+    local capabilities = require('blink-cmp').get_lsp_capabilities()
+    local lspconfig = require('lspconfig')
+    lspconfig.lua_ls.setup{capabilities = capabilities}
+    lspconfig.pyright.setup{capabilities = capabilities}
+    lspconfig.gopls.setup{capabilities = capabilities}
+  end
 }
