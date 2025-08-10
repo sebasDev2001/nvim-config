dbfabe744a2136c57dd56c7f7945c8056c9ec056
Sebastian Sierra V <sebastian.sierrav2001@gmail.com>
2025-08-10 12:59:31 -0500

    merge config with mac 1

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -23,7 +23,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.sql"},
     -- { import = "lazyvim.plugins.extras.lang.rust"},
-    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    -- { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.formatting.black" },
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,6 +8,9 @@ vim.g.autoformat = false
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
 vim.cmd([[let &t_Ce = "\e[4:0m"]])
 
+-- python options
+vim.g.lazyvim_python_lsp = "pyright"
+vim.g.lazyvim_python_ruff = "ruff"
 
 -- Line Numbers
 opt.relativenumber = true
